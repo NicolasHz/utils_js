4a809d9876261e9c9525622ff4ac1df35e599616
Nicolas Holzman <nicolasholzman@hotmail.com>
2018-10-29 16:58:58 -0300

    Update util.js

--- util.js
+++ util.js
@@ -2,3 +2,7 @@
     [arr[0], arr[arr.length - 1]] = [arr[arr.length - 1], arr[0]];
     return arr;
   }
+  shiftOne(arr, direction) {
+    // dont forget to create a copy of the array before if you dont want sideEffects
+    direction > 0 ? arr.unshift(arr.pop()) : arr.push(arr.shift())
+  }
