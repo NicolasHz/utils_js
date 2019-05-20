02e5ff33146572fb241a2816427459b261d02f98
Nicolas Holzman <nicolasholzman@hotmail.com>
2019-05-20 09:09:17 -0300

    Update util.js

--- util.js
+++ util.js
@@ -1,18 +1,21 @@
   swap(arr) {
-    [arr[0], arr[arr.length - 1]] = [arr[arr.length - 1], arr[0]];
-    return arr;
+	// dont forget to create a copy of the array before if you dont want sideEffects
+	  [arr[0], arr[arr.length - 1]] = [arr[arr.length - 1], arr[0]];
+	  return arr;
   }
   shiftOne(arr, direction) {
-    // dont forget to create a copy of the array before if you dont want sideEffects
-    direction > 0 ? arr.unshift(arr.pop()) : arr.push(arr.shift())
+	// dont forget to create a copy of the array before if you dont want sideEffects
+	  direction > 0 ? arr.unshift(arr.pop()) : arr.push(arr.shift());
+	  return arr;
   }
 
 function findMostReaptedWord(str){
     let counts = {}, mr=[];
 
     str.slice().match(/\w+/g).forEach(w => { counts[w]=(counts[w]||0)+1 });
+    const higger = Math.max.apply(null, Object.values(counts));
     for (let w in counts) {
-      if (counts[w] === Math.max.apply(null, Object.values(counts))) {
+      if (counts[w] === higger) {
 		mr = [...mr, {word: w, times:  counts[w]}]      
       }
     }
